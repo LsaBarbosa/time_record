c0e8757ec2852fe9424611ede6b2afe930c7e5e6
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-09-13 15:47:42 -0300

    Start project

---
+++ src/main/java/br/com/kornos/time_records/TimeRecordsApplication.java
@@ -0,0 +1,13 @@
+package br.com.kornos.time_records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeRecordsApplication.class, args);
+	}
+
+}
